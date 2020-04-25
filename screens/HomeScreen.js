997c4059cb397aa0a18711876b4fa0a086bb0196
Amaury Wyart <amauryw@bam.tech>
2020-04-25 14:27:40 +0200

    add-logo-in-home-page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,26 @@
 import * as React from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+
+const LOGO_HEIGHT = 80;
 
 export default function HomeScreen() {
   return (
     <View>
-      <Text>coucou</Text>
+      <View style={styles.headerContainer}>
+        <View style={styles.logoPlace}></View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headerContainer: {
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  logoPlace: {
+    width: LOGO_HEIGHT,
+    height: LOGO_HEIGHT,
+    backgroundColor: "blue"
+  }
+});
