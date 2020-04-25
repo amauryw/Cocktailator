0e13ef61d1a979ae546684a295f52f7d6da5457a
Amaury Wyart <amauryw@bam.tech>
2020-04-25 15:24:46 +0200

    add row

--- constants/Layout.js
+++ constants/Layout.js
@@ -1,12 +1,16 @@
-import { Dimensions } from 'react-native';
+import { Dimensions } from "react-native";
 
-const width = Dimensions.get('window').width;
-const height = Dimensions.get('window').height;
+const width = Dimensions.get("window").width;
+const height = Dimensions.get("window").height;
 
 export default {
   window: {
     width,
-    height,
+    height
   },
-  isSmallDevice: width < 375,
+  margin: {
+    small: 2,
+    medium: 8,
+  },
+  isSmallDevice: width < 375
 };
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,26 +1,65 @@
 import * as React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, ScrollView } from "react-native";
+import { CoctailCard } from "../components/CocktailCard";
 
 const LOGO_HEIGHT = 80;
 
 export default function HomeScreen() {
+  const renderHeader = () => {
+    return (
+      <>
+        <View style={styles.headerContainer}>
+          <View style={styles.logoPlace}></View>
+        </View>
+      </>
+    );
+  };
   return (
-    <View>
-      <View style={styles.headerContainer}>
-        <View style={styles.logoPlace}></View>
-      </View>
+    <View style={styles.mainContainer}>
+      {renderHeader()}
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.scrollViewContainer}>
+          <CoctailCard
+            label="coucou"
+            uri="https://www.thecocktaildb.com/images/media/drink/g1vnbe1493067747.jpg"
+          />
+          <CoctailCard
+            label="coucou"
+            uri="https://www.thecocktaildb.com/images/media/drink/g1vnbe1493067747.jpg"
+            inversed
+          />
+          <CoctailCard
+            label="coucou"
+            uri="https://www.thecocktaildb.com/images/media/drink/g1vnbe1493067747.jpg"
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1
+  },
   headerContainer: {
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    backgroundColor: "red"
   },
   logoPlace: {
     width: LOGO_HEIGHT,
     height: LOGO_HEIGHT,
     backgroundColor: "blue"
+  },
+  scrollView: {
+    flex: 1,
+    backgroundColor: "white",
+    flexDirection: "column"
+  },
+  scrollViewContainer: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "yellow"
   }
 });
