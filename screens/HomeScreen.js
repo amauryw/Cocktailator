10e3cec4f12daaaaabfd2bc27de15023fe2a689c
Amaury Wyart <amauryw@bam.tech>
2020-04-25 14:12:47 +0200

    add-nav

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>coucou</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
