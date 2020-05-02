3a287a0c634f3be073de5d72c32f84282510a8ce
arthur.picci <arthur.picci@gmail.com>
2020-05-03 01:24:47 +0200

    get instruction + ingredients from API

--- screens/RecipeScreen.js
+++ screens/RecipeScreen.js
@@ -2,26 +2,48 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { fetchCocktailByID } from "../api/cocktails.api";
+import { onSessionWasInterrupted } from "expo/build/AR";
 
 export default function RecipeScreen({ route, navigation }) {
-    const [currentCocktail, setCurrentCocktail] = useState(null);
+    const [currentCocktail, setCurrentCocktail] = useState({
+        cocktail: {
+            name: null,
+            instructions: null,
+            ingredients: [],
+        }
+    });
+    const [isLoading, setIsLoading] = useState(false);
     const currentCocktailId = route.params.id;
     useEffect(() => {
         async function fetchData() {
-            const data = await getData(currentCocktailId);
-            setCurrentCocktail(data.name);
+            try {
+                setIsLoading(true);
+                const data = await getData(currentCocktailId);
+                setIsLoading(false);
+                setCurrentCocktail(data);
+                console.log(data.ingredients);
+                console.log(currentCocktail.ingredients);
+            } catch (err) {
+                setIsLoading(false);
+            }
         }
         fetchData();
     }, []);
     return (
         <ScrollView>
             <Text>
-                ID cocktail =
                 {currentCocktailId}
             </Text>
             <Text>
-                {currentCocktail}
-                salut
+                {currentCocktail.name}
+                {isLoading ? "loading" : ""}
+            </Text>
+            <Text>
+                {currentCocktail.instructions}
+            </Text>
+            <Text>
+                {currentCocktail.ingredients[0].ingredient}
+                {currentCocktail.ingredients[0].measure}
             </Text>
         </ScrollView>
     );
@@ -29,12 +51,24 @@ export default function RecipeScreen({ route, navigation }) {
 
 const getData = async id => {
     try {
-        const apiFetchedCockail = await fetchCocktailByID(id);
+        const apiFetchedCocktail = await fetchCocktailByID(id);
+        let ingredients = [];
+        let currentIngredient;
+        let test;
+        let i = 1;
+        while (apiFetchedCocktail[0][`strMeasure${i}`] != null && i < 16) {
+            currentIngredient = { ingredient: apiFetchedCocktail[0][`strIngredient${i}`], measure: apiFetchedCocktail[0][`strMeasure${i}`] };
+            ingredients.push(currentIngredient);
+            i++;
+        }
         return {
-            name: apiFetchedCockail[0].strDrink,
+            name: apiFetchedCocktail[0].strDrink,
+            instructions: apiFetchedCocktail[0].strInstructions,
+            ingredients,
         }
     } catch (error) {
         console.log("error API call by ID", error);
+        throw error;
     }
 };
 
